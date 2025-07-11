4c9839018ca044b1a025e1f358c521247c858420
lnwchok <sitthichok.punlabpho@gmail.com>
2025-07-12 00:32:24 +0700

    Update sh_script

---
+++ sh_script/gitupload.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Error: No commit message found"
+	exit 1
+fi
+
+git add .
+git commit -m "$1"
+git push -u origin main
+echo "Thanks"
---
+++ sh_script/setup_tmux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+	echo "Error: Session name not specified "
+	exit 1
+fi
+
+SESSION=$1
+# Start New Session with our name
+tmux new-session -d -s $SESSION
+
+tmux split-window -hl 10
+tmux split-window -t2
+
+
+
+# Attach Session, on the Main window
+tmux attach-session -t $SESSION
+
